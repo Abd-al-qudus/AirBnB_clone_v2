e654683563ec376368371ce0ef53c3388223a6df
Abd-al-qudus <jimohqudus86@gmail.com>
2023-03-23 14:38:51 +0100

    corrected all files for hbnb

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,8 @@
--- a script that creates a mySql server for the project
+-- create database hbnb_dev_db if not exists
+-- create user hbnb_dev with pwd hbnb_dev_pwd
+-- grant hbnb_dev select on performance_schema
+-- grant all privileges on hbnb_dev_db
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
--- create new user in localhost if not exist with password hbnb_dev_pwd
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
--- grant privileges to the user
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,10 +1,8 @@
--- this script prepares a test MySQL server for the project
+-- create database hbnb_test_db if not exists
+-- create user hbnb_test with pwd hbnb_test_pwd
+-- grant hbnb_test select on performance_schema
+-- grant hbnb_test all privileges on hbnb_test_db
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
--- creating new user named : hbnb_test with all privileges on the db hbnb_test_db
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
--- granting the SELECT privilege for the user hbnb_test on the db performance_schema
-GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
--- granting all privileges to the new user on hbnb_test_db
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON performance_schema.* TO 'hbnb_test'@'localhost';
