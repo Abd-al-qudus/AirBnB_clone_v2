675fd909de8d0a66648a01dcb59ba0ceb0243a54
Abd-al-qudus <jimohqudus86@gmail.com>
2023-03-23 14:36:01 +0100

    updated mysql task for hbnb

--- models/state.py
+++ models/state.py
@@ -1,22 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-import models
 from models.base_model import BaseModel, Base
+from models import storage_type
+from models.city import City
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from os import environ as env
 
 
 class State(BaseModel, Base):
-    """ State class """
+    """ State class / table model"""
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship('City', cascade='all, delete', backref='state')
+    if storage_type == 'db':
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ''
 
-    if env.get('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            """filestorage relationship between state and city"""
-            lst = [val for key, val in models.storage.all(models.City).items()
-                   if val.state_id == self.id]
-            return lst
+            '''returns the list of City instances with state_id
+                equals the current State.id
+                FileStorage relationship between State and City
+            '''
+            from models import storage
+            related_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    related_cities.append(city)
+            return related_cities
